db64cbb9baf40da67d9890d1f84c69f0130eff54
juan camilo camacho <juancamilo.camacho@rappi.com>
2021-09-17 19:22:49 -0500

    main

---
+++ lib/models/articulo.model.dart
@@ -0,0 +1,18 @@
+class Articulo {
+  String author;
+  String title;
+  String description;
+  String url;
+  String urlToImage;
+  String publishedAt;
+  String content;
+  Articulo({
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+}
